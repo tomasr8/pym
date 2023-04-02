1344b19ccee3b8e0b13b5f1f4c0a7a1495104b3d
Tomas Roun <tomas.roun8@gmail.com>
2023-04-03 00:00:27 +0200

    Add a command to print a dummy config

--- pam_python/__init__.py
+++ pam_python/__init__.py
@@ -8,6 +8,10 @@ from pam_python.pam_python import Message, PamException, PamHandle, Response, XA
 __version__ = '0.0.0'
 __all__ = ['PamHandle', 'PamException', 'Message', 'Response', "XAuthData"]
 
+
+def _get_lib_path():
+    return (Path(__file__).parent / "pam_python.so").resolve()
+
 if __name__ == "__main__":
     @click.group()
     def cli():
@@ -16,6 +20,17 @@ if __name__ == "__main__":
     @cli.command()
     def libpath():
         """Get the shared lib path."""
-        click.echo((Path(__file__).parent / "pam_python.so").resolve())
+        click.echo(_get_lib_path())
+
+    @cli.command("print-config")
+    @click.argument("filename", type=click.Path(exists=True), help="Path to the Python PAM file")
+    def config(filename):
+        """Create and print a dummy PAM config file."""
+        lib_path = _get_lib_path()
+        file_path = Path(filename).resolve()
+        click.echo(f"account\trequired\t{lib_path} {file_path}")
+        click.echo(f"auth\trequired\t{lib_path} {file_path}")
+        click.echo(f"session\trequired\t{lib_path} {file_path}")
+        click.echo(f"password\trequired\t{lib_path} {file_path}")
 
     cli()
